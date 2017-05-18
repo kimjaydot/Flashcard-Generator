0fd1ca6aa8a5a230c6766ac0c1e0aee417bc9aa4
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-05-18 18:39:21 -0500

    need to rewrite code

--- app.js
+++ app.js
@@ -8,5 +8,26 @@ function BasicCard(){
 }
 
 BasicCard();
+//Only have 1 constructor in 1 file each
+//Gets properties (prototypes) basic card has front and back, also clozeCard has front and back
+//Object.getOwnPropertyNames(Object.prototype)
+
+
+//var example {
+    //front: Hi i Am George Washington
+//}
+//console.log(me.toString()));
+
+
+
+//argument means parameter
+function Person(firstName, lastNAme){
+    this.firstName = firstName;
+    this.lastName = lastNAme;
+}
+
+
+var me = new Person ("Erick", "Rivas");
+var me = new Person ("Darrell", "Freeman");
 
 
