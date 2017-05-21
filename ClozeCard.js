0b410b6a79da2aebc7142c0f03f029973bc11175
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-05-21 14:24:00 -0500

    create ClozeCard Function, add comments

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,6 +1,27 @@
 //Constructor Function For Cloze Card
 
-function ClozeCard(text, cloze){
+function ClozeCard(text, cloze, partial, fullText ){
     this.text = text;
     this.cloze = cloze;
+    this.partial = partial;
+    this.fullText = fullText;
 }
+
+
+
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington", "... was the first president of the United States.",  "George Washington was the first president of the United States.");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial); 
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText); 
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+// var brokenCloze("This doesn't work", "oops"); 
+
