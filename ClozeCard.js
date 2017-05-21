83012634c4c9997e94f210df94a922383aa824f3
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-05-21 15:16:45 -0500

    add property

--- ClozeCard.js
+++ ClozeCard.js
@@ -8,7 +8,7 @@ function ClozeCard(text, cloze, partial, fullText ){
     this.brokenCloze = "This doesn't work," + "oops";
 }
 
-var firstPresidentCloze = new ClozeCard( "George Washington", "... was the first president of the United States.",  "George Washington was the first president of the United States.");
+var firstPresidentCloze = new ClozeCard( "George Washington", "... was the first president of the United States.", "George Washington was the first president of the United States." , "George Washington was the first president of the United States.");
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 
