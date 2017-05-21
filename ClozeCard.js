7f9a003777b12060a9b061722b5e63201683c3a9
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-05-21 14:54:55 -0500

    add objects, firstPresidentCloze variable, comments

--- ClozeCard.js
+++ ClozeCard.js
@@ -7,21 +7,19 @@ function ClozeCard(text, cloze, partial, fullText ){
     this.fullText = fullText;
 }
 
-
-
-
-var firstPresidentCloze = new ClozeCard(
-    "George Washington", "... was the first president of the United States.",  "George Washington was the first president of the United States.");
-
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
-
-// " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); 
-
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText); 
+var firstPresidentCloze = new ClozeCard( "George Washington", "... was the first president of the United States.",  "George Washington was the first president of the United States." ,"This doesn't work", "oops");
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-// var brokenCloze("This doesn't work", "oops"); 
+
+//  var brokenCloze = new ClozeCard("This doesn't work", "oops"); 
+// "George Washington"
+// console.log(firstPresidentCloze.cloze); 
+
+// // " ... was the first president of the United States.
+// console.log(firstPresidentCloze.partial); 
+
+// // "George Washington was the first president of the United States.
+// console.log(firstPresidentCloze.fullText); 
+console.log(firstPresidentCloze);
+ClozeCard();
 
