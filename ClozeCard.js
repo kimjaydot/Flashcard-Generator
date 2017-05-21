e511d27bfef4eb397efeafe594e66dba2760ce5b
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-05-21 15:14:17 -0500

    add brokenCloze prototype

--- ClozeCard.js
+++ ClozeCard.js
@@ -5,9 +5,10 @@ function ClozeCard(text, cloze, partial, fullText ){
     this.cloze = cloze;
     this.partial = partial;
     this.fullText = fullText;
+    this.brokenCloze = "This doesn't work," + "oops";
 }
 
-var firstPresidentCloze = new ClozeCard( "George Washington", "... was the first president of the United States.",  "George Washington was the first president of the United States." ,"This doesn't work", "oops");
+var firstPresidentCloze = new ClozeCard( "George Washington", "... was the first president of the United States.",  "George Washington was the first president of the United States.");
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 
